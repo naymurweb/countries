67b5c7769e396c5ed0001c59287277a4d259256a
naymur <naymurweb@gmail.com>
2023-10-29 09:05:07 +0600

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-
 function App() {
-
   return (
     <>
-    <h1>setup</h1>
-     
+      <h1>setup</h1>
+
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn">Secondary</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
